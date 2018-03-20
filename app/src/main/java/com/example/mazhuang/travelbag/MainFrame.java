601e22aef75717533dea658800a31f7e20b1abcc
mz <1625128973@.com>
2018-03-20 18:20:53 +0800

    mainFrame

--- app/src/main/java/com/example/mazhuang/travelbag/MainFrame.java
+++ app/src/main/java/com/example/mazhuang/travelbag/MainFrame.java
@@ -2,12 +2,15 @@ package com.example.mazhuang.travelbag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainFrame extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_frame);
+
     }
 }
