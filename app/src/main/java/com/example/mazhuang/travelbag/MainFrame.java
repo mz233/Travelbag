d522add52d5eb679298cd4fa0583841b3d4cd716
mz <1625128973@.com>
2018-03-27 15:55:42 +0800

    second

--- app/src/main/java/com/example/mazhuang/travelbag/MainFrame.java
+++ app/src/main/java/com/example/mazhuang/travelbag/MainFrame.java
@@ -1,16 +1,26 @@
 package com.example.mazhuang.travelbag;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainFrame extends AppCompatActivity {
-
+    private Button button1;//上门取货
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_frame);
-
+        button1 = (Button) findViewById(R.id.doorBtn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainFrame.this, FetchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
